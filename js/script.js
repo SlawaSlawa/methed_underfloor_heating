57358a97982664d41c886a4fb1618c5914bdcb9c
SlawaSlawa <slawapocht4@yandex.ru>
2023-03-17 15:12:17 +0400

    day four

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Slider
+
 const slider = new Swiper('.hero__slider', {
     slidesPerView: 2,
     spaceBetween: 10,
@@ -21,5 +23,35 @@ const slider = new Swiper('.hero__slider', {
             slidesPerView: 2,
             spaceBetween: 10,
         },
+    }
+})
+
+// Calculator
+
+const calcForm = document.querySelector('.js-calc-form')
+const totalSquare = document.querySelector('.js-square')
+const totalPrice = document.querySelector('.js-total-price')
+const calcResultWrapper = document.querySelector('.js-calc-result-wrapper')
+
+const tariff = {
+    economy: 550,
+    comfort: 1400,
+    premium: 2700,
+}
+
+calcForm.addEventListener('submit', (evt) => {
+    evt.preventDefault()
+    const width = calcForm.width.value
+    const length = calcForm.length.value
+    const chosenTariff = calcForm.tariff.value
+
+    if (width > 0 && length >0) {
+        const square = width * length
+        const total = square * tariff[chosenTariff]
+        
+        totalSquare.textContent = `${square} кв м`
+        totalPrice.textContent = `${total} руб`
+
+        calcResultWrapper.style.display = 'block'
     }
 })
